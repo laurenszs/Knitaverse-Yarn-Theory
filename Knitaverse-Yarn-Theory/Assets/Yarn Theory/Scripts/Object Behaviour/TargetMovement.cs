61cc84c7c9e3027293c79a6898a02a3628f5f8c3
Chaunce1121 <99341504+Chaunce1121@users.noreply.github.com>
2022-05-10 11:22:31 +0200

    file verplaatsing toevoegingen

---
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/Object Behaviour/TargetMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TargetMovement : MonoBehaviour
+{
+    public float scrollSpeed;
+    [SerializeField] private float maxHeight = 25;
+    public float respawnHeight = 0;
+    private Vector3 spawnRight, spawnLeft;
+
+    private void Start()
+    {
+        var parent = gameObject.transform.parent;
+        spawnLeft = parent.GetComponentInParent<TargetSpawner>().leftBoundary.localPosition;
+        spawnRight = parent.GetComponentInParent<TargetSpawner>().rightBoundary.localPosition;
+    }
+
+    private void Update()
+    {
+        ScrollTarget();
+        RespawnTarget();
+    }
+
+    private void ScrollTarget()
+    {
+        var position = transform.localPosition;
+        transform.localPosition = new Vector3(position.x, position.y + scrollSpeed, position.z);
+    }
+
+    private void RespawnTarget()
+    {
+        var xPos = Random.Range(spawnLeft.x, spawnRight.x);
+        var zPos = Random.Range(spawnLeft.z, spawnRight.z);
+        if (transform.localPosition.y >= maxHeight) transform.localPosition = new Vector3(xPos, respawnHeight, zPos);
+    }
+}
