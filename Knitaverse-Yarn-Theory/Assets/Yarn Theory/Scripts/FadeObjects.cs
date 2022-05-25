744c6cbd0cf51a37614ab0d5491ec80a89abb3d7
Laurens <laurenscnossen@gmail.com>
2022-05-25 15:07:34 +0200

    extra lights + object fade script

---
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/FadeObjects.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FadeObjects : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float fadingDistance = 10f;
+
+    private float _collisionPoint;
+    [SerializeField] private Material fadingMaterial;
+    private bool _triggered;
+    private static readonly int Opacity = Shader.PropertyToID("_Opacity");
+
+    private void Start()
+    {
+        _collisionPoint = transform.localScale.x / 2;
+    }
+
+    private void Update()
+    {
+        var actualDistance =
+            math.distance(_collisionPoint, math.distance(transform.position, player.transform.position));
+
+        if (actualDistance <= fadingDistance)
+        {
+            fadingMaterial.SetFloat(Opacity, actualDistance / fadingDistance);
+        }
+        else
+        {
+            fadingMaterial.SetFloat(Opacity, 1);
+        }
+        
+        Debug.Log(actualDistance);
+    }
+}
