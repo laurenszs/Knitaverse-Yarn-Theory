039560c0b653b1f1b1f6cb5d87611fcf7bbc867b
JarmovanDongen <55130148+JarmovanDongen@users.noreply.github.com>
2022-06-08 14:52:58 +0200

    Changed OnDestroyLoad

--- Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/SceneChange.cs
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/SceneChange.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public GameObject player;
+    private void Awake()
+    {
+        DontDestroyOnLoad(player);
+    }
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(0);
