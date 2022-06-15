b1b6aa20e0f8acccb3368a7eebe4d242f294e4be
Chaunce1121 <yuen.chaunce@gmail.com>
2022-06-15 12:54:22 +0200

    offboarding bijna klaar

--- Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/TeleportingTopFloor.cs
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/TeleportingTopFloor.cs
@@ -6,16 +6,26 @@ public class TeleportingTopFloor : MonoBehaviour
 {
     public Transform player;
     public Transform teleportPosition;
+    public AudioSource audioSource;
+    public AudioClip teleporting;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            TeleportationPoint();
+            StartCoroutine(TeleportDelay());
+
         }
     }
     public void TeleportationPoint()
     {
         player.transform.position = teleportPosition.transform.position;
     }
- }
+
+    IEnumerator TeleportDelay()
+    {
+        audioSource.PlayOneShot(teleporting);
+        yield return new WaitForSeconds(1.5f);
+        TeleportationPoint();
+    }
+}
