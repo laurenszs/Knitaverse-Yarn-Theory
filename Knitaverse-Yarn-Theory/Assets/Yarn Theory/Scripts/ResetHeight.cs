a540e42e8419188246b96509b23981994dec9032
Chaunce1121 <55739548+laurenszs@users.noreply.github.com>
2022-06-15 12:26:17 +0200

    possible mirror floaty fix

--- Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/ResetHeight.cs
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/ResetHeight.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ResetHeight : MonoBehaviour
 {
-    [SerializeField] private float resetHeight;
+  //  [SerializeField] private float resetHeight;
 
     void Update()
     {
-        if (transform.localPosition.y < resetHeight) transform.localPosition = new Vector3(transform.localPosition.x, resetHeight, transform.localPosition.z);
+        //if (transform.localPosition.y < resetHeight) transform.localPosition = new Vector3(transform.localPosition.x, resetHeight, transform.localPosition.z);
 
         if (transform.parent.tag != "Hands") GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
     }
