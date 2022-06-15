411621a3528ef9de686755be2c21ef8232470baa
Chaunce1121 <55739548+laurenszs@users.noreply.github.com>
2022-06-15 12:25:44 +0200

    skybox fade half, spiegel fixed + mats, balls room

---
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/ResetHeight.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetHeight : MonoBehaviour
+{
+    [SerializeField] private float resetHeight;
+
+    void Update()
+    {
+        if (transform.localPosition.y < resetHeight) transform.localPosition = new Vector3(transform.localPosition.x, resetHeight, transform.localPosition.z);
+
+        if (transform.parent.tag != "Hands") GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
