7f2e0cb868d83c79a6716e517471dce2338aac02
Chaunce1121 <55739548+laurenszs@users.noreply.github.com>
2022-06-20 14:44:23 +0200

    hub fade + lighting + hub onboarding

---
+++ Knitaverse-Yarn-Theory/Assets/Yarn Theory/Scripts/FadePoint.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FadePoint : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float fadingDistance = 10f;
+
+    [SerializeField] private string materialProperty;
+    [SerializeField] private GameObject fadeObject;
+    private Material _fadingMaterial;
+    private float _collisionPoint;
+    public float fadeOffset = .3f;
+
+    private void Start()
+    {
+        _collisionPoint = transform.lossyScale.x / 2;
+        _fadingMaterial = fadeObject.GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        var actualDistance =
+            math.distance(player.transform.position, transform.position);
+
+        if (actualDistance <= fadingDistance && ((actualDistance / fadingDistance) - fadeOffset) >= 0)
+        {
+            _fadingMaterial.SetFloat(materialProperty, (actualDistance / fadingDistance) - fadeOffset);
+        }
+        else if (actualDistance > fadingDistance)
+        {
+            _fadingMaterial.SetFloat(materialProperty, 1);
+        }
+    }
+}
